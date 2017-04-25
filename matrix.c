1c5c96124d9b4630f32506ee03d2f224241d13d6
wangzhihao <1397665447@qq.com>
2017-04-26 07:42:14 +0800

    4.25

--- matrix.c
+++ matrix.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 #include"matrix.h"
+#include"vector.h"
 
 matrix addm(matrix a,matrix b){
     matrix res;
@@ -55,4 +56,48 @@ void set_zero(matrix *m){
             m -> m[i][j] = 0.0f;
         }
     }
+}
+vector vmulmat(vector v,matrix m){
+    vector res;
+    res.x = v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + v.w * m.m[3][0];
+	res.y = v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + v.w * m.m[3][1];
+	res.z = v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + v.w * m.m[3][2];
+	res.w = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + v.w * m.m[3][3];
+    return res;
+}
+void trans_mat(matrix *m,float x,float y,float z){
+    set_identity(m);
+    m->m[3][0]=x;
+    m->m[3][1]=y;
+    m->m[3][2]=z;
+}
+void scale_mat(matrix *m,float x,float y,float z){
+    set_identity(m);
+    m->m[0][0]=x;
+    m->m[1][1]=y;
+    m->m[2][2]=z;
+}
+void totate_x_mat(matrix *m,float a){
+    set_zero(m);
+    m->m[0][0]=m->m[3][3]=1;
+    m->m[1][1]=cos(a);
+    m->m[2][2]=cos(a);
+    m->m[1][2]=-sin(a);
+    m->m[2][1]=sin(a);   
+}
+void totate_y_mat(matrix *m,float a){
+    set_zero(m);
+    m->m[1][1]=m->m[3][3]=1;
+    m->m[0][0]=cos(a);
+    m->m[0][3]=sin(a);
+    m->m[2][0]=-sin(a);
+    m->m[2][2]=cos(a);
+}
+void totate_z_mat(matrix *m,float a){
+    set_zero(m);
+    m->m[2][2]=m->m[3][3]=1;
+    m->m[0][0]=cos(a);
+    m->m[0][1]=-sin(a);
+    m->m[1][0]=sin(a);
+    m->m[1][1]=cos(a);
 }
--- matrix.h
+++ matrix.h
@@ -1,6 +1,7 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
+#include"vector.h"
 typedef struct {
     float m[4][4];
 }matrix;
@@ -11,6 +12,13 @@ matrix mulnum(float f,matrix m);
 matrix mulmatr(matrix a,matrix b);
 void set_identity(matrix *m);
 void set_zero(matrix *m);
+vector vmulmat(vector v,matrix m);
+
+void trans_mat(matrix *m,float x,float y,float z);
+void scale_mat(matrix *m,float x,float y,float z);
+void totate_x_mat(matrix *m,float a);
+void totate_y_mat(matrix *m,float a);
+void totate_z_mat(matrix *m,float a);
 
 
 
